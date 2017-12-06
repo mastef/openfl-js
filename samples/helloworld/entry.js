48d8fb6fe22be67c7849b0d40dbc189c4ca8fcab
Joshua Granick <jgranick@users.noreply.github.com>
2017-12-05 22:19:46 -0800

    Add Hello World sample

---
+++ samples/helloworld/entry.js
@@ -0,0 +1,12 @@
+var BitmapData = require ("openfl/src/openfl/display/BitmapData").default;
+var Rectangle = require ("openfl/src/openfl/geom/Rectangle").default;
+
+//
+
+var bitmapData = new BitmapData (200, 200, true, 0xFF24AFC4);
+
+var rect = new Rectangle (50, 50, 100, 100);
+bitmapData.fillRect (rect, 0xFFCCCCCC);
+
+document.body.appendChild (bitmapData.image.src);
+console.log ("Hello World");
