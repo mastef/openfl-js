a4aa33fe4b6237a4de65a7eb699197a5690e4ab5
Joshua Granick <jgranick@users.noreply.github.com>
2017-12-05 22:31:25 -0800

    Add TypeScript Hello World

---
+++ samples/helloworld-ts/entry.ts
@@ -0,0 +1,12 @@
+import BitmapData from "openfl/src/openfl/display/BitmapData";
+import Rectangle from "openfl/src/openfl/geom/Rectangle";
+
+//
+
+var bitmapData = new BitmapData (200, 200, true, 0xFF24AFC4);
+
+var rect = new Rectangle (50, 50, 100, 100);
+bitmapData.fillRect (rect, 0xFFCCCCCC);
+
+document.body.appendChild (bitmapData.image.src);
+console.log ("Hello World");
